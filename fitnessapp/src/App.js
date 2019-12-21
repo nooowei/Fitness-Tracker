499764b169ef9fd5988258a062cc51e1c0e424f9
unknown <wei.xw.xu@gmail.com>
2019-12-21 13:23:11 -0500

    created routes for react

--- fitnessapp/src/App.js
+++ fitnessapp/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
+//importing the components from the folder
 import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
+// using Router to determind which component to load into the div
 function App() {
   return (
     <Router>
