24beaf769d5408a38375c180dca1ecaf7e1814ca
unknown <wei.xw.xu@gmail.com>
2019-12-23 10:40:08 -0500

    Changed the structure of app to backend including frontend

--- fitnessapp/server.js
+++ fitnessapp/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -21,8 +22,21 @@ connection.once('open', () => {
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
+//use the routes
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+
+//serve static asset if in production
+if(process.env.NODE_ENV === 'production'){
+  console.log("Production environment. Serving static asset from client/build.");
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // server will serve up index.html in the build folder if in production
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
